45b8c39db39ada31e7a7e4204e7baa05cc864dbe
coolbox13 <61984046+coolbox13@users.noreply.github.com>
2023-10-04 19:49:16 +0200

    Update percentage_to_knx_bus.ino

--- percentage_to_knx_bus.ino
+++ percentage_to_knx_bus.ino
@@ -1,27 +1,31 @@
+// Optimized and standardized ESP8266 version for KNX communication
+
 #include <esp-knx-ip.h>
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
-#include <ESP8266WebServer.h>  // Include the ESP8266WebServer library
 
-// WiFi credentials
-const char* ssid = "coolbox_down";
-const char* password = "1313131313131";
+// Constants
+const char* SSID = "ssid";               // WiFi SSID
+const char* PASSWORD = "password";          // WiFi Password
+const uint32_t SERIAL_BAUD_RATE = 115200;        // Serial communication baud rate
+const uint32_t WIFI_CONNECTION_DELAY = 1000;     // Delay in ms while waiting for WiFi connection
+const uint32_t LOOP_DELAY = 10000;               // Delay in ms for the main loop
 
-address_t ph_addr = {};   // empty assigment to change default 1.1.0 into 1.1.201
+// Global variables
+address_t ph_addr = {};                          // Physical address for KNX
+uint8_t counter = 40;                            // Initial value for dummy percentage
 
-uint8_t counter = 40; //start value for dummy percentage
-
-void setKNXPhysicalAddress (int area, int line, int member) {
-    // We want to send a counter to 3/0/16
+// Function to set the KNX physical address
+void setKNXPhysicalAddress(int area, int line, int member) {
     address_t receiving_pa;
     receiving_pa.pa.area = area;
     receiving_pa.pa.line = line;
     receiving_pa.pa.member = member;
-    // Set the physical address
-    knx.physical_address_set(receiving_pa);
+    knx.physical_address_set(receiving_pa);      // Set the physical address for KNX
 }
 
-address_t calculate_ga (int area, int line, int member) {
+// Function to calculate the group address for KNX
+address_t calculate_ga(int area, int line, int member) {
     address_t tmp;
     tmp.ga.area = area;
     tmp.ga.line = line;
@@ -29,35 +33,36 @@ address_t calculate_ga (int area, int line, int member) {
     return tmp;
 }
 
+// Setup function
 void setup() {
+    // Start serial communication
+    Serial.begin(SERIAL_BAUD_RATE);
 
-  Serial.begin(115200);
+    // Connect to WiFi
+    Serial.println("Connecting to WiFi...");
+    WiFi.begin(SSID, PASSWORD);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(WIFI_CONNECTION_DELAY);
+        Serial.println("Waiting for WiFi connection...");
+    }
+    Serial.print("Connected to WiFi!\nIP Address: ");
+    Serial.println(WiFi.localIP());
 
-  // Connect to WiFi
-  Serial.println("Connecting to WiFi...");
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Waiting for WiFi connection...");
-  }
-  Serial.print("Connected to WiFi!\nIP Address: ");
-  Serial.println(WiFi.localIP());
+    // Start KNX communication
+    knx.start(nullptr);                          // Start KNX loop without webserver
 
-  // Start knx
-  knx.start(nullptr); //knx loop started without webserver
-
-  // set physical address to 1.1.201
-  setKNXPhysicalAddress(1,1,201);
+    // Set KNX physical address
+    setKNXPhysicalAddress(1, 1, 201);
 }
 
-
-
+// Main loop function
 void loop() {
-  knx.loop();
+    knx.loop();                                  // Continue KNX communication loop
 
-  knx.write_1byte_uint(calculate_ga(3,0,15), counter);    // send value to knx address 3/0/15
+    // Send counter value to KNX address
+    knx.write_1byte_uint(calculate_ga(3, 0, 15), counter);
 
-  counter++;
+    counter++;                                   // Increment counter
 
-  delay(10000); // Wait one second
+    delay(LOOP_DELAY);                           // Wait for the specified delay
 }
